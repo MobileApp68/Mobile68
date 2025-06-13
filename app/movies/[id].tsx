73be033569a7e76f05d443c9b126726a2897b32c
Kobby97 <kobinaassan97@gmail.com>
2025-06-13 08:49:42 +0000

    I addedd the bottom navigation page

---
+++ app/movies/[id].tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Menus = () => {
+  return (
+    <View>
+      <Text>Menus</Text>
+    </View>
+  )
+}
+
+export default Menus
+
+const styles = StyleSheet.create({})
